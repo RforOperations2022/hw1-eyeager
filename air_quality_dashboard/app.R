995d3a8b7182d85beefb47febf225bbf19635102
emmayeager <eyeager@andrew.cmu.edu>
2022-02-04 10:52:23 -0500

    Add nitrogen dioxide plot

--- air_quality_dashboard/app.R
+++ air_quality_dashboard/app.R
@@ -1,27 +1,29 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
+# This is a Shiny web application for visualizing air quality indicators by New
+# York City neighborhood. You can run the application by clicking the 'Run App' 
+# button above.
 #
 
+# Set up
 library(shiny)
 library(tidyverse)
-df <- read.csv(file = 'data/air_quality.csv')
-df$X <- as.character(df$X)
-df$start_date <- as.Date(df$start_date)
-df$name <- as.factor(df$name)
-df$geo_place_name <- as.factor(df$geo_place_name)
 
-# Define UI for application that draws a histogram
+# Load and clean air quality data
+# See air_quality_dashboard/data subfolder for more information on the data 
+# source 
+df <- read.csv(file = 'data/air_quality.csv')
+df$X <- as.character(df$X) # row index
+df$start_date <- as.Date(df$start_date) # annual data 
+df$name <- as.factor(df$name) # name of air quality indicator
+df$geo_place_name <- as.factor(df$geo_place_name) # name of NYC neighborhood
+
+# Define UI for application that plots air quality data
 ui <- fluidPage(
 
     # Application title
-    titlePanel("NYC Air Quality Data"),
+    titlePanel("NYC Air Quality Data by Neighborhood"),
 
-    # Sidebar with a slider input for number of bins 
+    # Sidebar with inputs for neighborhood and plot titles
     sidebarLayout(
         sidebarPanel(
             selectInput(inputId = "geo",
@@ -31,30 +33,45 @@ ui <- fluidPage(
                         selected = "Upper West Side")
         ),
 
-        # Show a plot of the generated distribution
+        # Show a plots of the fine paticulate matter and nitrogen dioxide 
+        # indicators by neighborhood
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("pmPlot"),
+           plotOutput("no2Plot")
         )
     )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw air quality plots
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # draw the histogram with the specified number of bins
-        #hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    output$pmPlot <- renderPlot({
         
-        myColors <- c('#9C2E35', rep('#D3D3D3', 33))
-        print(myColors)
+        # Create a color scale that highlights the neighborhood selected by user
+        # https://stackoverflow.com/questions/6919025/how-to-assign-colors-to-categorical-variables-in-ggplot2-that-have-stable-mappin
+        myColors <- c('#9C2E35', rep('#D3D3D3', 33)) # selected is red, all else gray
         names(myColors) <- c(input$geo, purrr::discard(levels(df$geo_place_name),.p = ~stringr::str_detect(.x,input$geo)))
-        print(myColors)
+        # https://stackoverflow.com/questions/48467884/remove-an-element-of-a-list-by-name]
         colScale <- scale_colour_manual(name = "geo_place_name",values = myColors)
-        
+
+        # Subset the particulate matter data
         pm <- subset(df, name == 'Fine Particulate Matter (PM2.5)')
         
-        # Line plot of particulate matter
+        # Draw line plot of particulate matter
         ggplot(data=pm, aes(x=start_date, y=data_value, group=geo_place_name, color=geo_place_name)) +
+            geom_line()+
+            geom_point()+
+            colScale+
+            theme(legend.position = 'none')
+    })
+    
+    output$no2Plot <- renderPlot({
+        
+        # Subset the particulate matter data
+        no2 <- subset(df, name == 'Nitrogen Dioxide (NO2)')
+        
+        # Dtaw line plot of particulate matter
+        ggplot(data=no2, aes(x=start_date, y=data_value, group=geo_place_name, color=geo_place_name)) +
             geom_line()+
             geom_point()+
             colScale+
