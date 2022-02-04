b640edab3bb8e8cf4ba0cbe91f9adc240aa84505
emmayeager <eyeager@andrew.cmu.edu>
2022-02-04 01:11:07 -0500

    Add neighborhood input for particulate matter plot

--- air_quality_dashboard/app.R
+++ air_quality_dashboard/app.R
@@ -8,8 +8,12 @@
 #
 
 library(shiny)
-library(ggplot2)
+library(tidyverse)
 df <- read.csv(file = 'data/air_quality.csv')
+df$X <- as.character(df$X)
+df$start_date <- as.Date(df$start_date)
+df$name <- as.factor(df$name)
+df$geo_place_name <- as.factor(df$geo_place_name)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -20,11 +24,11 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectInput(inputId = "geo",
+                        label = "Neighborhood:",
+                        #choices = as.vector(levels(df$geo_place_name)),
+                        choices = c("West Queens", "Upper West Side", "Rockaways"),
+                        selected = "Upper West Side")
         ),
 
         # Show a plot of the generated distribution
@@ -38,19 +42,23 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
         # draw the histogram with the specified number of bins
         #hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        
+        myColors <- c('#9C2E35', rep('#D3D3D3', 33))
+        print(myColors)
+        names(myColors) <- c(input$geo, purrr::discard(levels(df$geo_place_name),.p = ~stringr::str_detect(.x,input$geo)))
+        print(myColors)
+        colScale <- scale_colour_manual(name = "geo_place_name",values = myColors)
         
         pm <- subset(df, name == 'Fine Particulate Matter (PM2.5)')
         
         # Line plot of particulate matter
-        ggplot(data=pm, aes(x=start_date, y=data_value, group=geo_place_name)) +
+        ggplot(data=pm, aes(x=start_date, y=data_value, group=geo_place_name, color=geo_place_name)) +
             geom_line()+
-            geom_point()
+            geom_point()+
+            colScale+
+            theme(legend.position = 'none')
     })
 }
 
